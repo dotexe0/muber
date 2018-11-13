48e324eff7bd35a27aa9f423623f7cb234fde658
Daniel Varela <dvarela@wiser.com>
2018-11-13 08:54:23 -0800

    add bodyparser to middleware

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,7 @@ module.exports = app => {
   // Listening for incoming requests of GET methods
 // to the route http://localhost:3050/api
   app.get('/api', DriversController.greeting)
+  
+  app.post('/api/drivers', DriversController.create)
 }
 
